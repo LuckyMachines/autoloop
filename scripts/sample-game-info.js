25c8cbb4f6f6ba8a7ee6cf1e5e1ba07528568d2f
Max Meier <max@studiomax.io>
2023-01-09 15:36:41 -0800

    sample game info script

---
+++ scripts/sample-game-info.js
@@ -0,0 +1,31 @@
+const hre = require("hardhat");
+const fs = require("fs");
+const deployments = require("../deployments.json");
+require("dotenv").config();
+
+async function main() {
+  if (!deployments[hre.network.name].GAME_LOOP_REGISTRAR) {
+    console.log(
+      "\nRegistrar not deployed. Run the deployment script or set the address in deployments.json first.\n"
+    );
+  } else if (!deployments[hre.network.name].SAMPLE_GAME) {
+    console.log("\n Sample game not deployed.\n");
+  } else {
+    const Game = await hre.ethers.getContractFactory("NumberGoUp");
+    const game = Game.attach(deployments[hre.network.name].SAMPLE_GAME);
+    const gameNumber = await game.number();
+    const gameInterval = await game.interval();
+    const gameLastTimeStamp = await game.lastTimeStamp();
+    console.log(`Current Game State for ${game.address}:`);
+    console.log(
+      `#:${gameNumber.toString()}\ninterval:${gameInterval}\nlast time stamp:${gameLastTimeStamp}`
+    );
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
