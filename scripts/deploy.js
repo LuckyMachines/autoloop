d4a2d9f8b02b9d723e6abf0907c7a6a9fbb2efaf
Max Meier <max@studiomax.io>
2023-01-08 17:31:37 -0800

    setup scripts for hardhat

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,12 @@
-// deploy contracts
+const hre = require("hardhat");
+
+async function main() {
+  // deploy contracts
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- scripts/register-controller.js
+++ scripts/register-controller.js
@@ -1,1 +1,12 @@
-// register controller with registrar contract
+const hre = require("hardhat");
+
+async function main() {
+  // register controller with registrar contract
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
