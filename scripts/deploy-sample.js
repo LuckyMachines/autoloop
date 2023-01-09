c272ef13a1806a4e75e456646a7eb27f711a51b2
Max Meier <max@studiomax.io>
2023-01-09 15:29:28 -0800

    save sample game to deployments

--- scripts/deploy-sample.js
+++ scripts/deploy-sample.js
@@ -3,7 +3,28 @@ const fs = require("fs");
 const deployments = require("../deployments.json");
 require("dotenv").config();
 
+class Deployment {
+  constructor() {
+    this.deployments = deployments;
+    this.path = `${process.cwd()}/deployments.json`;
+  }
+  save() {
+    fs.writeFileSync(
+      this.path,
+      JSON.stringify(this.deployments, null, 4),
+      (err) => {
+        if (err) {
+          console.log("error: unable to save deployments file");
+        } else {
+          console.log("Saved deployments");
+        }
+      }
+    );
+  }
+}
+
 async function main() {
+  const deployment = new Deployment();
   if (!deployments[hre.network.name].GAME_LOOP_REGISTRAR) {
     console.log(
       "\nRegistrar not deployed. Run the deployment script or set the address in deployments.json first.\n"
@@ -19,10 +40,13 @@ async function main() {
 
     // Register game loop...
     let tx = await game.registerGameLoop(
-      deployments[hre.network.name].GAME_LOOP_REGISTRAR
+      deployment.deployments[hre.network.name].GAME_LOOP_REGISTRAR
     );
     await tx.wait();
     console.log("Game loop registered.");
+
+    deployment.deployments[hre.network.name].SAMPLE_GAME = game.address;
+    deployment.save();
   }
 }
 
