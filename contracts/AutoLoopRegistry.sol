54c8dd7c78b5ce1941b60a64a692cfc3b1ade56b
Max Meier <max@studiomax.io>
2023-03-25 13:16:52 -0700

    get registered auto loops for address

--- contracts/AutoLoopRegistry.sol
+++ contracts/AutoLoopRegistry.sol
@@ -2,15 +2,23 @@
 pragma solidity ^0.8.7;
 
 import "./AutoLoopRoles.sol";
+import "./AutoLoopCompatible.sol";
 
 contract AutoLoopRegistry is AutoLoopRoles {
+    // Mappings from registered AutoLoop or Controller Addresses
     mapping(address => bool) public isRegisteredAutoLoop;
     mapping(address => bool) public isRegisteredController;
 
     mapping(address => uint256) _registeredAutoLoopIndex;
     mapping(address => uint256) _registeredControllerIndex;
+
+    // All registered AutoLoops
     address[] _registeredAutoLoops;
     address[] _registeredControllers;
+
+    // mapping from admin to _registeredAutoLoops indices
+    // this is a historical record, doesn't indicate current admin status
+    mapping(address => uint256[]) _registeredAutoLoopsForAddress;
 
     event AutoLoopRegistered(
         address autoLoopAddress,
@@ -62,6 +70,36 @@ contract AutoLoopRegistry is AutoLoopRoles {
         }
     }
 
+    function getRegisteredAutoLoopsFor(
+        address adminAddress
+    ) public view returns (address[] memory autoLoops) {
+        uint256 totalRegistrations = 0;
+        uint256[] memory registeredLoops = _registeredAutoLoopsForAddress[
+            adminAddress
+        ];
+        for (uint256 i = 0; i < registeredLoops.length; i++) {
+            AutoLoopCompatible compatibleContract = AutoLoopCompatible(
+                _registeredAutoLoops[registeredLoops[i]]
+            );
+            if (compatibleContract.hasRole(DEFAULT_ADMIN_ROLE, adminAddress)) {
+                ++totalRegistrations;
+            }
+        }
+        autoLoops = new address[](totalRegistrations);
+        uint256 outputIndex = 0;
+        for (uint256 i = 0; i < registeredLoops.length; i++) {
+            AutoLoopCompatible compatibleContract = AutoLoopCompatible(
+                _registeredAutoLoops[registeredLoops[i]]
+            );
+            if (compatibleContract.hasRole(DEFAULT_ADMIN_ROLE, adminAddress)) {
+                autoLoops[outputIndex] = _registeredAutoLoops[
+                    registeredLoops[i]
+                ];
+                ++outputIndex;
+            }
+        }
+    }
+
     function getRegisteredControllers()
         public
         view
@@ -85,6 +123,7 @@ contract AutoLoopRegistry is AutoLoopRoles {
 
     // Cleanup
     // TODO: remove zero addresses from registration lists
+    // Careful here, will need to re-map any registered autoloops to new indices
     function cleanControllerList() public {}
 
     function cleanAutoLoopList() public {}
@@ -127,6 +166,9 @@ contract AutoLoopRegistry is AutoLoopRoles {
         _registeredControllerIndex[registrantAddress] =
             _registeredControllers.length -
             1;
+        _registeredAutoLoopsForAddress[registrantAddress].push(
+            _registeredControllerIndex[registrantAddress]
+        );
         emit ControllerRegistered(
             registrantAddress,
             msg.sender,
@@ -148,6 +190,32 @@ contract AutoLoopRegistry is AutoLoopRoles {
                 msg.sender,
                 block.timestamp
             );
+        }
+    }
+
+    function setNewAdmin(
+        address autoLoopCompatibleContract,
+        address adminAddress
+    ) external onlyRole(REGISTRAR_ROLE) {
+        uint256 autoLoopIndex = _registeredAutoLoopIndex[
+            autoLoopCompatibleContract
+        ];
+        uint256[] memory existingRegistrations = _registeredAutoLoopsForAddress[
+            adminAddress
+        ];
+        bool registrationExists;
+        for (uint256 i = 0; i < existingRegistrations.length; i++) {
+            if (
+                _registeredAutoLoops[existingRegistrations[i]] ==
+                autoLoopCompatibleContract
+            ) {
+                // already in registered list
+                registrationExists = true;
+                break;
+            }
+        }
+        if (!registrationExists) {
+            _registeredAutoLoopsForAddress[adminAddress].push(autoLoopIndex);
         }
     }
 }
