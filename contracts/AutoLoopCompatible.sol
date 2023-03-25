babbf4006659478e5a134827e95a5cc7861ec259
Max Meier <max@studiomax.io>
2023-03-25 13:31:40 -0700

    safe transfer admin

--- contracts/AutoLoopCompatible.sol
+++ contracts/AutoLoopCompatible.sol
@@ -7,4 +7,31 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 abstract contract AutoLoopCompatible is
     AutoLoopCompatibleInterface,
     AccessControlEnumerable
-{}
+{
+    address _adminTransferRequest;
+
+    function safeTransferAdmin(address newAdminAddress) public {
+        require(
+            newAdminAddress != address(0),
+            "cannot transfer admin to zero address"
+        );
+        _adminTransferRequest = newAdminAddress;
+    }
+
+    function acceptTransferAdminRequest() public {
+        require(
+            _msgSender() == _adminTransferRequest,
+            "Only new admin can accept transfer request"
+        );
+        _setupRole(DEFAULT_ADMIN_ROLE, _adminTransferRequest);
+        _adminTransferRequest = address(0);
+    }
+
+    function cancelTransferAdminRequest() public {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, _msgSender()),
+            "Only current admin can cancel transfer request"
+        );
+        _adminTransferRequest = address(0);
+    }
+}
