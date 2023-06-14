769b5810cafdff97c89ceabd81625cc60b52c242
Max Meier <max@studiomax.io>
2023-06-13 18:24:12 -0700

    update gas buffer

---
+++ scripts/update-gas-buffer.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+const deployments = require("../deployments.json");
+
+async function main() {
+  // register controller with registrar contract
+  const AutoLoop = await hre.ethers.getContractFactory("AutoLoop");
+  const autoLoop = AutoLoop.attach(deployments[hre.network.name].AUTO_LOOP);
+  const tx = await autoLoop.setGasBuffer("149440");
+  await tx.wait();
+  console.log("Gas Buffer Updated");
+  const gasBuffer = await autoLoop.gasBuffer();
+  console.log("Gas Buffer:", gasBuffer.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
